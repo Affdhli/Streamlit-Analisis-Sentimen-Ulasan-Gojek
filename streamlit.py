d085b44e18608a548ca016e49ec06655c340d4e3
20-088_Achmad Fany Fadheli <108218918+Affdhli@users.noreply.github.com>
2025-06-28 17:29:54 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -126,7 +126,7 @@ elif app_mode == "Preprocessing":
                 data['case_folded'] = data['cleaned_text'].str.lower()
                 
                 # Tokenisasi
-                data['tokens'] = data['case_folded'].apply(tokenize_text)
+                data['tokens'] = data['case_folded'].apply(word_tokenize)
                 
                 # Stopword removal
                 data['filtered_tokens'] = data['tokens'].apply(remove_stopwords)
